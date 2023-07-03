d291fed7ab02537548bab5cc9d9dcb237c2c3980
abdrahman02 <abdul.190180078@mhs.unimal.ac.id>
2023-07-03 17:31:18 +0700

    memperbaiki nama aplikasi dan tema

--- lib/main.dart
+++ lib/main.dart
@@ -10,73 +10,74 @@ class Catatan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplikasi Catatan Sederhana',
-      theme: ThemeData(primarySwatch: Colors.brown),
-      home: DaftarCatatan(),
+      title: 'Aplikasi Agenda Kegiatan Sederhana',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: DaftarAgenda(),
     );
   }
 }
 
-class DaftarCatatan extends StatefulWidget {
-  const DaftarCatatan({super.key});
+class DaftarAgenda extends StatefulWidget {
+  const DaftarAgenda({super.key});
 
   @override
-  State<DaftarCatatan> createState() => _DaftarCatatanState();
+  State<DaftarAgenda> createState() => _DaftarAgendaState();
 }
 
-class _DaftarCatatanState extends State<DaftarCatatan> {
-  List<String> catatans = [];
+class _DaftarAgendaState extends State<DaftarAgenda> {
+  List<String> agendas = [];
 
-  void tambahCatatan(String catatan) {
+  void tambahAgenda(String agenda) {
     setState(() {
-      catatans.add(catatan);
+      agendas.add(agenda);
     });
   }
 
-  void doneCatatan(int index) {
+  void doneAgenda(int index) {
     setState(() {
-      catatans.removeAt(index);
+      agendas.removeAt(index);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Catatan')),
+      appBar: AppBar(title: Text('Agenda')),
       body: ListView.builder(
-          itemCount: catatans.length,
+          itemCount: agendas.length,
           itemBuilder: (context, index) {
             return ListTile(
-                title: Text(catatans[index]),
-                trailing: IconButton(
-                  icon: Icon(Icons.check),
-                  onPressed: () => doneCatatan(index),
-                ));
+              title: Text(agendas[index]),
+              trailing: IconButton(
+                icon: Icon(Icons.check),
+                onPressed: () => doneAgenda(index),
+              ),
+            );
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          final catatan = await showDialog(
+          final agenda = await showDialog(
               context: context,
               builder: (BuildContext context) {
-                String newCatatan = '';
+                String newAgenda = '';
                 return AlertDialog(
-                    title: Text('Catatan Baru'),
+                    title: Text('Agenda Baru'),
                     content: TextField(
                       onChanged: (value) {
-                        newCatatan = value;
+                        newAgenda = value;
                       },
                     ),
                     actions: [
                       ElevatedButton(
                           onPressed: () {
-                            Navigator.of(context).pop(newCatatan);
+                            Navigator.of(context).pop(newAgenda);
                           },
                           child: Text('Tambah'))
                     ]);
               });
 
-          if (catatan != null) {
-            tambahCatatan(catatan);
+          if (agenda != null) {
+            tambahAgenda(agenda);
           }
         },
         child: Icon(Icons.add),
